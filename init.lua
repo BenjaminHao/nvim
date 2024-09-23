8e4161b93ba40a58ff2ba54c0b4d480bf8da423b
Benjamin Hao <haomingkai@gmail.com>
2024-09-23 12:51:56 -0700

    chore: change couple text

--- init.lua
+++ init.lua
@@ -1,17 +1,20 @@
 --╭──────────────────────────────────────────────────────────────────────────╮--
 --│                                                                          │--
---│  MODULE: nvim                                                            │--
+--│  MODULE: nvim init                                                       │--
 --│  DETAIL: Entry point of Neovim config                                    │--
 --│  CREATE: 2024-08-08 by Benjamin Hao                                      │--
---│  UPDATE: 2024-08-08 by Benjamin Hao                                      │--
+--│  UPDATE: 2024-09-20 by Benjamin Hao                                      │--
 --│                                                                          │--
 --╰──────────────────────────────────────────────────────────────────────────╯--
-require("my.configs.autocmds").setup()
-require("my.configs.options").setup()
+-- Configs
 require("my.configs.systems").setup()
+require("my.configs.options").setup()
+require("my.configs.autocmds").setup()
+-- Keymaps
 require("my.keymaps.core").setup()
 require("my.keymaps.ui").setup()
+require("my.keymaps.lsp").setup()
 require("my.keymaps.tools").setup()
 require("my.keymaps.editor").setup()
-require("my.keymaps.completion").setup()
+-- Plugins
 require("my.plugins.manager").setup()

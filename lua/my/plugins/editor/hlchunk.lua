6f6c07eaf4125372e349564691fb39e7ad491d4c
Benjamin Hao <haomingkai@gmail.com>
2024-10-03 14:17:23 -0700

    feat: config hlchunk

--- lua/my/plugins/editor/hlchunk.lua
+++ lua/my/plugins/editor/hlchunk.lua
@@ -13,32 +13,38 @@ local Plugin = {
 }
 
 Plugin.config = function()
+  local colors = require("catppuccin.palettes").get_palette("mocha")
+
   require("hlchunk").setup({
     chunk = {
       enable = true,
-      notify = true,
-      hl_group = {
-        chunk = "Yellowfg",
-        error = "Error",
+      style = {
+        { fg = colors.sky },  -- for chunk indicator
+        { fg = colors.red },  -- for error indicator
       },
       chars = {
         horizontal_line = "─",
         vertical_line = "│",
         left_top = "╭",
         left_bottom = "╰",
-        -- left_arrow = "<",
-        bottom_arrow = "v",
         right_arrow = ">",
       },
-      textobject = "ah",
-      animate_duration = 60,
-      fire_event = { "CursorHold", "CursorHoldI" },
+      error_sign = false,
+      duration = 60,
+      delay = 300;
     },
     indent = {
       enable = false,
+      style =  colors.sky,
+      chars = { "│" },
+    },
+    blank = {
+      enable = false,
+      chars = { "·" },
     },
     line_num = {
-      enable = false,
+      enable = true,
+      style = colors.sky,
     },
   })
 end
